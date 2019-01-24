a71646c0e716ddc80578d6793e4d8523294c815c
Dirk Mahler <dirk.mahler@buschmais.com>
2019-01-24 08:53:11 +0100

    fixed checkstyle violations

--- src/main/java/com/buschmais/jqassistant/plugin/m2repo/api/model/MavenSnapshotDescriptor.java
+++ src/main/java/com/buschmais/jqassistant/plugin/m2repo/api/model/MavenSnapshotDescriptor.java
@@ -2,7 +2,6 @@ package com.buschmais.jqassistant.plugin.m2repo.api.model;
 
 import com.buschmais.jqassistant.plugin.maven3.api.model.MavenDescriptor;
 import com.buschmais.xo.neo4j.api.annotation.Label;
-import com.buschmais.xo.neo4j.api.annotation.Property;
 
 /**
  * Represents a snapshot artifact in a maven repository.
